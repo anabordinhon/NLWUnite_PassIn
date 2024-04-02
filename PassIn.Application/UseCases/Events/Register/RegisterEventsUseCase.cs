f0a21247fe3048b4368565464c6566b414acb4ca
anabordinhon <anabordinhon@outlook.com.br>
2024-04-01 23:48:44 -0300

    Add project files.

---
+++ PassIn.Application/UseCases/Events/Register/RegisterEventsUseCase.cs
@@ -0,0 +1,51 @@
+﻿using PassIn.Communication.Requests;
+using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+using PassIn.Infrastructure;
+using PassIn.Infrastructure.Entities;
+
+namespace PassIn.Application.UseCases.Events.Register
+{
+    public class RegisterEventsUseCase
+    {
+        public ResponseRegisteredEventJson Execute(RequestEventJson request)
+        {
+            Validate(request);
+            var dbContext = new PassInDbContext();
+            var entity = new Event
+            {
+                Title = request.Title,
+                Details = request.Details,
+                Maximum_Attendees = request.MaximumAttendees,
+                Slug = request.Title.ToLower().Replace(" ", "-"),
+            };
+
+            dbContext.Events.Add(entity);
+            dbContext.SaveChanges();
+
+            return new ResponseRegisteredEventJson
+            {
+                Id = entity.Id
+            };
+
+        }
+
+        private void Validate(RequestEventJson request)
+        {
+            if(request.MaximumAttendees <= 0)
+            {
+                throw new PassInException("The Maximum attendees is invalid.");
+            }
+
+            if (string.IsNullOrWhiteSpace(request.Title) || string.IsNullOrEmpty(request.Title))
+            {
+                throw new PassInException("The title is invalid.");
+            }
+
+            if (string.IsNullOrWhiteSpace(request.Details) || string.IsNullOrEmpty(request.Details))
+            {
+                throw new PassInException("The detail is invalid.");
+            }
+        }
+    }
+}
